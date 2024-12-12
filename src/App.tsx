fb84e1eee36aeb581de4e037875f0fef79703201
acnormun <anaclara.noronha@weni.ai>
2024-12-12 08:59:18 -0300

    chore: add router and redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import { SettingUp } from './pages/SettingUp';
 import { AgentBuilder } from './pages/AgentBuilder';
 import { Channels } from './pages/setup/Channels';
 import { Dashboard } from './pages/Dashboard';
+import { Provider } from 'react-redux';
+
+import store from './store/user.store';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <>
-      <SettingUp />
-      <AgentBuilder />
-      <Channels />
-      <Dashboard />
-    </>
+    <Provider store={store}>
+      <Router>
+      <Routes>
+      <Route path='/' element={<SettingUp />}></Route>
+      <Route path='/agent-builder' element={<AgentBuilder />}></Route>
+      <Route path='/channels' element={<Channels />}></Route>
+      <Route path='/dash' element={<Dashboard />}></Route>
+      </Routes>
+      </Router>
+    </Provider>
   )
 }
 
