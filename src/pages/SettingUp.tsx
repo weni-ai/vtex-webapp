4ea102f6b93247c42c5058455e3f7392ed108f3f
acnormun <anaclara.noronha@weni.ai>
2024-12-12 08:59:18 -0300

    chore: add router and redux

--- src/pages/SettingUp.tsx
+++ src/pages/SettingUp.tsx
@@ -1,7 +1,46 @@
 import { Center, Divider, Heading, Page, PageContent, PageHeader, PageHeaderRow, PageHeading, Spinner, Text } from '@vtex/shoreline'
 import WeniLogo from '../assets/weni-logo.svg'
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setUser } from '../store/userSlice';
 
 export function SettingUp() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSetName = async () => {
+    let userData;
+     try {
+    const response = await fetch('http://vtex-io.apip.stg.cloud.weni.ai/create_user', {
+      method: 'GET',
+      credentials: 'include',
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+  
+    userData = await response.json();
+    console.log('Dados do usuário:', userData);
+    dispatch(setUser(userData))
+    navigate('/agent-builder')
+  } catch (error) {
+    console.error('Erro ao buscar dados do usuário:', error);
+  }
+  };
+
+  let loggedIn = false
+  useEffect(() => {
+    if(!loggedIn){
+      handleSetName()
+    }
+    return () => {
+      loggedIn = true;
+    };
+
+  }, [navigate]);
+
+
   return (
     <Page>
       <PageHeader>
