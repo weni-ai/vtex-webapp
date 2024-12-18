75d8a2c204b9235a21eb8b5cab07ed0231f0220c
acnormun <anaclara.noronha@weni.ai>
2024-12-18 18:58:00 -0300

    fix: watch response

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,6 +8,7 @@ export function getUserFromLocalStorage() {
 export async function fetchUserData() {
   try {
     const { data } = await VTEXFetch('/api/vtexid/pub/authenticated/user');
+    console.log('entrou na service: ', data)
     return data;
   } catch (error) {
     console.error('Error:', error);
