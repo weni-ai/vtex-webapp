b8dbb0e87b1f00b3097be9df62c505f74167ec66
acnormun <anaclara.noronha@weni.ai>
2024-12-30 17:42:12 -0300

    fix: set env

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,8 @@ export JSON_STRING='window.configs = { \
   "SENTRY_DSN":"'${SENTRY_DSN}'", \
   "SENTRY_ENVIRONMENT":"'${SENTRY_ENVIRONMENT}'", \
   "API_BASE_URL":"'${API_BASE_URL}'", \
+  "FACEBOOK_APP_ID":"'${FACEBOOK_APP_ID}'", \
+  "WHATSAPP_FACEBOOK_APP_CONFIG_ID":"'${WHATSAPP_FACEBOOK_APP_CONFIG_ID}'", \
 }'
 sed "s|\/\/ CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/vtex-webapp/index.html.tmpl > /tmp/index.html
 
