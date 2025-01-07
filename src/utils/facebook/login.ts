caf40c5afbecb22155fd8434af7febd9c8ce6fe7
acnormun <anaclara.noronha@weni.ai>
2025-01-07 19:39:44 -0300

    chore: integrate feature

--- src/utils/facebook/login.ts
+++ src/utils/facebook/login.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { createChannel } from "../../services/channel.service";
 import getEnv from "../env";
-import { VTEXFetch } from "../VTEXFetch";
 
 function initFacebookSDK(appId: string, loginCallback: () => void) {
     window.fbAsyncInit = function () {
@@ -105,26 +105,4 @@ export function startFacebookLogin(project_uuid: string, token: string) {
     initFacebookSDK(fbAppId, loginCallback);
 }
 
-async function createChannel(code: string, project_uuid: string, wabaId: string, phoneId: string, token: string) {
 
-    const data = {
-        waba_id: wabaId,
-        phone_number_id: phoneId,
-        project_uuid: project_uuid,
-        auth_code: code,
-    };
-
-    console.log("Creating channel with data:", data);
-
-    await VTEXFetch(`/_v/whatsapp-integration?token=${token}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      }).then((response) => {
-        console.log('Whatsapp registered', response)
-      }).catch((error) => {
-        console.error('Error:', error);
-      });
-}
