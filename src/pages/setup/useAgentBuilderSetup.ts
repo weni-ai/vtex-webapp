dbcd61d77c898a742de90bc242ee91023471819c
acnormun <anaclara.noronha@weni.ai>
2025-01-08 14:35:26 -0300

    fix: validate url

--- src/pages/setup/useAgentBuilderSetup.ts
+++ src/pages/setup/useAgentBuilderSetup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useNavigate } from 'react-router-dom';
 import { setAgentBuilder } from '../../services/agent.service';
