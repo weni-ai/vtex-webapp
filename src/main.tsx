10ff97f73b39be771607910eb84b2d30449ba440
acnormun <anaclara.noronha@weni.ai>
2025-01-10 11:34:07 -0300

    chore: add wpp integration translation

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@vtex/shoreline/css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import store from './store/provider.store.ts'
+import i18n from 'i18next';
+import './i18n'; 
+
+(globalThis as any).t = (key: string, options?: Record<string, unknown>) => {
+  return i18n.t(key, options);
+};
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
