7f55f16776edf988ab2f3a8dd530a279df702240
acnormun <anaclara.noronha@weni.ai>
2025-01-13 14:11:49 -0300

    fix: get base address

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@vtex/shoreline/css'
 import App from './App.tsx'
-import { Provider } from 'react-redux'
+import { Provider, useDispatch } from 'react-redux'
 import store from './store/provider.store.ts'
 import i18n from 'i18next';
 import './i18n'; 
+import { setBaseAddress } from './store/authSlice.ts'
 
 (globalThis as any).t = (key: string, options?: Record<string, unknown>) => {
   return i18n.t(key, options);
 };
 
 window.addEventListener('message', (event) => {
-  if (event.data.name === 'SetLocalStorage') {
-    console.log('LocalStorage recebido:', event.data.data);
+  const dispatch = useDispatch()
+  if (event.data.name === 'SetLocalStorage' && event.data.data?.base_address) {
+    dispatch(setBaseAddress(event.data.data?.base_address))
   }
 });
 
