95f8ba45eded8b5dc3f842c999199230a48ada09
acnormun <anaclara.noronha@weni.ai>
2025-01-13 17:28:34 -0300

    fix: get base address and locale

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@vtex/shoreline/css'
-import App from './App.tsx'
-import { Provider, useDispatch } from 'react-redux'
-import store from './store/provider.store.ts'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@vtex/shoreline/css';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import store from './store/provider.store.ts';
 import i18n from 'i18next';
-import './i18n'; 
-import { setBaseAddress } from './store/authSlice.ts'
+import './i18n';
+import { setBaseAddress } from './store/authSlice.ts';
 
 (globalThis as any).t = (key: string, options?: Record<string, unknown>) => {
   return i18n.t(key, options);
 };
 
-window.addEventListener('message', (event) => {
-  const dispatch = useDispatch()
-  if (event.data.name === 'SetLocalStorage' && event.data?.base_address) {
-    dispatch(setBaseAddress(event.data.data?.base_address))
-  }
-});
+const params = new URLSearchParams(window.location.search);
+const baseAddress = params.get('base_address');
+const locale = params.get('locale');
+
+if (baseAddress) {
+  store.dispatch(setBaseAddress(baseAddress));
+}
+
+if (locale) {
+  i18n.changeLanguage(locale).catch(err => console.error('Erro ao mudar o idioma:', err));
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
