4ccd6247f240ae7cfd9e68e6de5c2e73e9d90e02
acnormun <anaclara.noronha@weni.ai>
2025-01-13 18:20:33 -0300

    chore: bump version 0.0.1-alpha115-staging

--- src/main.tsx
+++ src/main.tsx
@@ -7,23 +7,30 @@ import { Provider } from 'react-redux';
 import store from './store/provider.store.ts';
 import i18n from 'i18next';
 import './i18n';
-// import { setBaseAddress } from './store/authSlice.ts';
+import { setBaseAddress } from './store/authSlice.ts';
 
 (globalThis as any).t = (key: string, options?: Record<string, unknown>) => {
   return i18n.t(key, options);
 };
 
-const params = window.location.search;
-console.log('Base Address Decodificado:', params, decodeURIComponent(params));
+const query = decodeURIComponent(window.location.search);
+const params = new URLSearchParams(query);
+console.log('Base Address Decodificado:', params);
 
-// if (baseAddress) {
-//   console.log('alooo', baseAddress)
-//   store.dispatch(setBaseAddress(baseAddress));
-// }
+const baseAddress = params.get('base_address');
+const locale = params.get('locale');
 
-// if (locale) {
-//   i18n.changeLanguage(locale).catch(err => console.error('Erro ao mudar o idioma:', err));
-// }
+console.log('Base Address:', baseAddress);
+console.log('Locale:', locale);  
+
+if (baseAddress) {
+  console.log('alooo', baseAddress)
+  store.dispatch(setBaseAddress(baseAddress));
+}
+
+if (locale) {
+  i18n.changeLanguage(locale).catch(err => console.error('Erro ao mudar o idioma:', err));
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
