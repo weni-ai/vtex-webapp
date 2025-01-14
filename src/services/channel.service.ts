7b9d83a2eb2e5c8322bd135dfce8e45aade30f92
acnormun <anaclara.noronha@weni.ai>
2025-01-14 14:25:54 -0300

    chore: bump version 0.0.1-alpha120-staging

--- src/services/channel.service.ts
+++ src/services/channel.service.ts
@@ -1,14 +1,14 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useDispatch, useSelector } from "react-redux";
+import {useSelector } from "react-redux";
 import { VTEXFetch } from "../utils/VTEXFetch";
-import { setLoadingWhatsAppIntegration, setWhatsAppError, setWhatsAppIntegrated } from "../store/userSlice";
+// import { setLoadingWhatsAppIntegration, setWhatsAppError, setWhatsAppIntegrated } from "../store/userSlice";
 import { toast } from "@vtex/shoreline";
 import { selectBaseAddress } from "../store/authSlice";
 
 export async function createChannel(code: string, project_uuid: string, wabaId: string, phoneId: string, token: string) {
-  console.log('entrou no create channel...')
-  const dispatch = useDispatch();
-  console.log('setou o dispatch...')
+  console.log('entrou no create channel...', code, project_uuid, wabaId, phoneId, token)
+  // const dispatch = useDispatch();
+  // console.log('setou o dispatch...')
   const base_address = useSelector(selectBaseAddress)
   console.log('base address', base_address)
   const data = {
@@ -28,8 +28,8 @@ export async function createChannel(code: string, project_uuid: string, wabaId: 
     body: JSON.stringify(data),
   }).then(async (response) => {
     console.log('Whatsapp registered', response)
-    dispatch(setWhatsAppIntegrated(true))
-    dispatch(setLoadingWhatsAppIntegration(true))
+    // dispatch(setWhatsAppIntegrated(true))
+    // dispatch(setLoadingWhatsAppIntegration(true))
     const integrateData = {
       project_uuid: project_uuid,
       store: base_address,
@@ -43,7 +43,7 @@ export async function createChannel(code: string, project_uuid: string, wabaId: 
       body: JSON.stringify(integrateData),
     })
   }).catch((error) => {
-    dispatch(setWhatsAppError(error))
+    // dispatch(setWhatsAppError(error))
     console.error('Error:', error);
     toast.critical(t('integrations.channel.whatsapp.error'));
   });
