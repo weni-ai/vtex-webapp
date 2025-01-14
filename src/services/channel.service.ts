570cc972bab424bb5734d4c13de7894be4531309
acnormun <anaclara.noronha@weni.ai>
2025-01-14 15:45:36 -0300

    fix: use dispatch

--- src/services/channel.service.ts
+++ src/services/channel.service.ts
@@ -1,14 +1,14 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useDispatch } from "react-redux";
+import store from "../store/provider.store";
 import { VTEXFetch } from "../utils/VTEXFetch";
 import { setLoadingWhatsAppIntegration, setWhatsAppError, setWhatsAppIntegrated } from "../store/userSlice";
 import { toast } from "@vtex/shoreline";
-import store from "../store/provider.store";
 
 export async function createChannel(code: string, project_uuid: string, wabaId: string, phoneId: string, token: string) {
-  console.log('entrou no create channel...')
-  const dispatch = useDispatch();
-  const base_address = store.getState().auth.base_address
+  console.log('entrou no create channel...', code, project_uuid, wabaId, phoneId, token);
+
+  const base_address = store.getState().auth.base_address; // Acessa diretamente o estado
+  console.log('chegou aqui', base_address);
+
   const data = {
     waba_id: wabaId,
     phone_number_id: phoneId,
@@ -18,31 +18,37 @@ export async function createChannel(code: string, project_uuid: string, wabaId: 
 
   console.log("Creating channel with data:", data);
 
-  await VTEXFetch(`/_v/whatsapp-integration?token=${token}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  }).then(async (response) => {
-    console.log('Whatsapp registered', response)
-    dispatch(setWhatsAppIntegrated(true))
-    dispatch(setLoadingWhatsAppIntegration(true))
+  try {
+    const response = await VTEXFetch(`/_v/whatsapp-integration?token=${token}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log('Whatsapp registered', response);
+
+    store.dispatch(setWhatsAppIntegrated(true)); // Usa a store para despachar ações
+    store.dispatch(setLoadingWhatsAppIntegration(true));
+
     const integrateData = {
       project_uuid: project_uuid,
       store: base_address,
       flows_channel_uuid: response.flow_object_uuid,
-      wpp_cloud_app_uuid: response.data.app_uuid
-    }
+      wpp_cloud_app_uuid: response.data.app_uuid,
+    };
+
     await VTEXFetch(`/_v/integrate-available-features?token=${token}`, {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(integrateData),
-    })
-  }).catch((error) => {
-    dispatch(setWhatsAppError(error))
+    });
+  } catch (error) {
+    store.dispatch(setWhatsAppError(error)); // Usa a store para despachar ações
     console.error('Error:', error);
-    toast.critical(t('integrations.channel.whatsapp.error'));
-  });
+    toast.critical('Integrations channel WhatsApp error');
+  }
 }
