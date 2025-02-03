5bbed025e9fc1f131ee9888e5f2606391527a737
acnormun <anaclara.noronha@weni.ai>
2025-02-03 18:31:19 -0300

    fix: token service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,19 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import getEnv from "../utils/env";
 
-export async function getToken() {
+export async function getToken() : Promise<any> {
     const auth_url = getEnv('VITE_APP_AUTH_URL') || '';
     const client_id = getEnv('VITE_APP_CLIENT_ID') || '';
     const client_secret = getEnv('VITE_APP_CLIENT_SECRET') || '';
+
+    if (!auth_url || !client_id || !client_secret) {
+        console.error('Erro: Configurações de autenticação ausentes.');
+        return { success: false, error: 'Configuração de autenticação ausente.' };
+    }
 
     const headersList = {
         "Accept": "*/*",
         "User-Agent": "Thunder Client (https://www.thunderclient.com)",
         "Content-Type": "application/x-www-form-urlencoded"
-    }
+    };
 
     const bodyContent = `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`;
 
-    if (auth_url) {
+    try {
         const response = await fetch(auth_url, {
             method: "POST",
             body: bodyContent,
@@ -21,7 +27,14 @@ export async function getToken() {
         });
 
         const data = await response.json();
-        return data.access_token
+
+        if (!response.ok) {
+            return { success: false, error: data.error_description || `Erro ${response.status}` };
+        }
+
+        return { success: true, token: data.access_token };
+    } catch (error) {
+        console.error('Erro ao obter token:', error);
+        return { success: false, error: error || 'Erro desconhecido' };
     }
 }
-
