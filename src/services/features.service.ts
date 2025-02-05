db31fb6dbfb34fa5d8d1e7159175e3c20297acaa
acnormun <anaclara.noronha@weni.ai>
2025-02-05 16:43:50 -0300

    chore: add integrate feature api

--- src/services/features.service.ts
+++ src/services/features.service.ts
@@ -21,9 +21,43 @@ export async function getFeatureList(project_uuid: string, token: string) {
   }
 }
 
-export async function integrateAvailableFeatures(project_uuid: string, token: string) {
-  console.log('Token recebido:', token);
+export async function integrateFeature(feature_uuid: string, project_uuid: string, token: string) {
+  const store = storeProvider.getState().auth.base_address;
+  const flows_channel_uuid = storeProvider.getState().project.flows_channel_uuid;
+  const wpp_cloud_app_uuid = storeProvider.getState().project.wpp_cloud_app_uuid;
+
+  const data = {
+    feature_uuid,
+    project_uuid,
+    store,
+    flows_channel_uuid,
+    wpp_cloud_app_uuid,
+  };
   
+
+  try {
+    const response = await VTEXFetch(`/_v/integrate-feature?${token}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response || response.error) {
+      throw new Error(response?.message || 'Erro ao integrar funcionalidades.');
+    }
+
+    console.log('Funcionalidades integradas com sucesso:', response);
+
+    return { success: true, data: response };
+  } catch (error) {
+    console.error('Erro ao integrar funcionalidades:', error);
+    return { success: false, error: error|| 'Erro desconhecido' };
+  }
+}
+
+export async function integrateAvailableFeatures(project_uuid: string, token: string) {
   const store = storeProvider.getState().auth.base_address;
   const flows_channel_uuid = storeProvider.getState().project.flows_channel_uuid;
   const wpp_cloud_app_uuid = storeProvider.getState().project.wpp_cloud_app_uuid;
@@ -36,7 +70,7 @@ export async function integrateAvailableFeatures(project_uuid: string, token: st
   };
 
   try {
-    const response = await VTEXFetch(`/_v/integrate-available-features?`, {
+    const response = await VTEXFetch(`/_v/integrate-available-features?token=${token}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
