93d333235e6f66eceb28cb321d2bcd1fd1ce4a3b
Matheus Cristian <mathues@email.com>
2025-02-25 09:12:07 -0300

    feat(adpter-agents-list): adds requests and adpter functions

---
+++ src/api/agents/requests.ts
@@ -0,0 +1,47 @@
+import { useSelector } from "react-redux";
+import { VTEXFetch } from "../../utils/VTEXFetch";
+import { selectProject } from "../../store/projectSlice";
+import {
+  AgentsListResponse,
+  adapterAgentsList,
+  IntegratedAgentsListResponse,
+  adapterIntegratedAgentsList,
+} from "./adpters";
+
+export async function agentsList() {
+  const projectUuid = useSelector(selectProject);
+
+  const queryParams = new URLSearchParams({
+    projectUUID: projectUuid
+  });
+
+  const url = `/_v/get-feature-list?${queryParams.toString()}`;
+
+  const response = await VTEXFetch<AgentsListResponse>(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return adapterAgentsList(response);
+}
+
+export async function integratedAgentsList() {
+  const projectUuid = useSelector(selectProject);
+
+  const queryParams = new URLSearchParams({
+    projectUUID: projectUuid
+  });
+
+  const url = `/_v/get-integrated-features?${queryParams.toString()}`;
+
+  const response = await VTEXFetch<IntegratedAgentsListResponse>(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return adapterIntegratedAgentsList(response);
+}
