cd77743ca9a327090f2beb4ba3a57cf7045ea832
Matheus Cristian <mathues@email.com>
2025-03-19 16:21:16 -0300

    typing(Facebook): adds typing for Facebook global object

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,9 +2,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="vite/client" />
 
-
 declare global {
-  declare var FB: any;
+  declare var FB: {
+    init: (config: {
+      appId: string;
+      xfbml: boolean;
+      version: string;
+    }) => void;
+
+    login: (
+      callback: (response: {
+        authResponse?: {
+          code: string;
+        };
+      }) => void,
+      options?: {
+        config_id: string;
+        response_type: string;
+        override_default_response_type: boolean;
+        extras: {
+          sessionInfoVersion: number;
+        };
+      }
+    ) => void;
+  }
+
   var fbAsyncInit;
 
 }
