d3269756c91b542b5d19237ede633a4e3729acd8
Matheus Cristian <mathues@email.com>
2025-03-19 17:02:27 -0300

    typing(agent-builder-setup): adds typing into agent builder setup

--- src/pages/setup/useAgentBuilderSetup.ts
+++ src/pages/setup/useAgentBuilderSetup.ts
@@ -1,19 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useNavigate } from 'react-router-dom';
 import { setAgentBuilder } from '../../services/agent.service';
 import { useSelector } from 'react-redux';
 import { toast } from '@vtex/shoreline';
 import store from '../../store/provider.store';
-import { selectProject, setAgentLoading } from '../../store/projectSlice';
-import { integrateFeature } from '../../services/features.service';
+import { selectProject, setAgentBuilderLoading } from '../../store/projectSlice';
+import { integrateAgent } from '../../services/agent.service';
 
 export function useAgentBuilderSetup() {
     const navigate = useNavigate();
     const project = useSelector(selectProject) || ''
     
     const buildAgent = async (payload: any, app_uuid: string) => {
-        store.dispatch(setAgentLoading(true))
+        store.dispatch(setAgentBuilderLoading(true))
         const cleanedPayload = Object.fromEntries(
             Object.entries(payload).filter(([_, value]) => value !== null && value !== undefined && value !== '')
         );
@@ -37,15 +35,15 @@ export function useAgentBuilderSetup() {
             toast.critical(t('agent.error'));
         } else {
             toast.success(t('agent.success'))
-            const orderStatus = store.getState().project.featureList.find(item => item.code === 'order_status')?.uuid
+            const orderStatus = store.getState().project.agents.find(item => item.code === 'order_status')?.uuid
             if (orderStatus) {
-                const integrateResponse = await integrateFeature(orderStatus, project)
+                const integrateResponse = await integrateAgent(orderStatus, project)
                 if(integrateResponse?.error){
                     toast.critical(t('integration.error'));
                 }
             }
 
-            store.dispatch(setAgentLoading(false))
+            store.dispatch(setAgentBuilderLoading(false))
             navigate('/dash');
         }
     };
