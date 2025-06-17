7e0ae368518747190560b68c102ad344f1c7ac21
Matheus Cristian <mathues@email.com>
2025-06-17 15:27:36 -0300

    feat(onboarding): adds onboarding page

--- src/pages/agent/AgentBuilder.tsx
+++ src/pages/agent/AgentBuilder.tsx
@@ -1,228 +1,95 @@
 import {
-  Button,
-  Container,
   Field,
   FieldError,
   Flex,
-  IconArrowLeft,
-  IconButton,
   Input,
   Label,
-  Page,
-  PageContent,
-  PageHeader,
-  PageHeaderRow,
-  PageHeading,
-  Radio,
-  RadioGroup,
-  Spinner,
-  Text,
   Textarea,
-  Tooltip,
+  Tooltip
 } from '@vtex/shoreline';
-import { ChangeEvent, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { agentBuilderLoading, getAgentBuilder, loadingSetup, setStoreType } from '../../store/projectSlice';
-import { isAgentBuilderIntegrated, isWhatsAppIntegrated, selectAccount } from '../../store/userSlice';
-import { useAgentBuilderSetup } from '../setup/useAgentBuilderSetup';
-import { useUserSetup } from '../setup/useUserSetup';
-import { AgentBuilderSkeleton } from './AgentBuilderSkeleton';
-import { Channel } from '../Channel';
-import { useNavigate } from 'react-router-dom';
-import question from '../../assets/icons/question.svg'
+import question from '../../assets/icons/question.svg';
+import { loadingSetup } from '../../store/projectSlice';
 import { cleanURL } from '../../utils';
-import store from '../../store/provider.store';
-import { VTEXFetch } from '../../utils/VTEXFetch';
+import { AgentBuilderSkeleton } from './AgentBuilderSkeleton';
 
-interface FormState {
+export interface FormState {
   name: string;
   knowledge: string;
   occupation: string;
   objective: string;
-  channel: string;
 }
 
-async function updateStoreType(storeType: string) {
-  const projectUuid = store.getState().project.project_uuid;
-
-  await VTEXFetch('/_v/set-vtex-store-type', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      "projectUUID": projectUuid,
-      "vtexStoreType": storeType,
-    }),
-  });
-
-  store.dispatch(setStoreType(storeType));
-}
-
-export function AgentBuilder() {
-  const agentBuilder = useSelector(getAgentBuilder);
-  const account = useSelector(selectAccount);
-  const [form, setForm] = useState<FormState>({
-    name: agentBuilder.name || account?.accountName || '',
-    knowledge: agentBuilder.links[0] || '',
-    occupation: agentBuilder.occupation || t('agent.setup.forms.occupation.default'),
-    objective: agentBuilder.objective || t('agent.setup.forms.objective.default'),
-    channel: store.getState().project.storeType || '',
-  });
-  const [errors, setErrors] = useState<{ [key in keyof FormState]?: string }>({});
-  const isWppIntegrated = useSelector(isWhatsAppIntegrated);
+export function AgentBuilder({ form, setForm, errors }: { form: FormState, setForm: (form: FormState) => void, errors: { [key in keyof FormState]?: string } }) {
   const isSetupLoading = useSelector(loadingSetup);
-  const isAgentBuilderLoading = useSelector(agentBuilderLoading)
-  const agentIntegrated = useSelector(isAgentBuilderIntegrated)
-  const { buildAgent } = useAgentBuilderSetup();
-  const { initializeUser } = useUserSetup();
-  const navigate = useNavigate()
-  
-  const initialize = async () => {
-    await initializeUser();
-
-    const shouldUpdateAgentBuilder = agentBuilder.name !== form.name;
-
-    if (shouldUpdateAgentBuilder) {
-      const payload = Object.fromEntries(
-        Object.entries(form).filter(([, value]) => value.trim())
-      ) as FormState;
-  
-      buildAgent(payload, false);
-    }
-  };
-  
-  useEffect(() => {
-    initialize();
-  }, [initializeUser]);
-
-  const isValidURL = (url: string) => {
-    const urlPattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]{2,}([/?#].*)?$/i;
-    return urlPattern.test(url);
-  };
-
-  const validateForm = () => {
-    const newErrors: { [key in keyof FormState]?: string } = {
-      name: !form.name.trim() ? t('agent.setup.forms.error.empty_input') : '',
-      knowledge: !form.knowledge.trim()
-        ? ''
-        : !isValidURL(form.knowledge.trim())
-          ? t('agent.setup.forms.error.valid_url')
-          : '',
-    };
-    setErrors(newErrors);
-    return !Object.values(newErrors).some((error) => error);
-  };
+  // const agentIntegrated = useSelector(isAgentBuilderIntegrated)
+  const agentIntegrated = false;
 
   const handleInputChange = (field: keyof FormState, value: string) => {
     if (field === 'knowledge') {
       value = cleanURL(value);
     }
 
-    setForm((prev) => ({ ...prev, [field]: value }));
-  };
-
-  const handleSubmit = async () => {
-    if (validateForm()) {
-      const payload = Object.fromEntries(
-        Object.entries(form).filter(([, value]) => value.trim())
-      ) as FormState;
-  
-      buildAgent(payload);
-  
-      await updateStoreType(form.channel);
-    }
+    setForm({ ...form, [field]: value });
   };
 
   return (
-    <Container>
-      <Page style={{ height: '100vh' }}>
-        <PageHeader>
-          <PageHeaderRow style={{ justifyContent: 'space-between', alignItems: 'center' }}>
-            <PageHeading style={{ display: 'flex', alignItems: 'center' }}>
-              <IconButton label='' variant="tertiary">
-                <IconArrowLeft onClick={() => navigate('/terms-and-conditions')} />
-              </IconButton>
-              <Text>{t('common.new_agent')}</Text>
-            </PageHeading>
-            <Button variant="primary" size="large" onClick={handleSubmit} disabled={isAgentBuilderLoading || !form.channel}>
-              {isAgentBuilderLoading ? <Spinner description="loading" /> : <span>{t('common.create')}</span>}
-            </Button>
-          </PageHeaderRow>
-        </PageHeader>
-        <PageContent style={{ maxWidth: '720px', padding: 0 }}>
-          <Flex direction="row" gap="var(--space-5, 20px)" style={{ marginBottom: 'var(--space-5, 20px)' }}>
-            <RadioGroup
-              label={t('agent.setup.forms.channel.title')}
-              defaultValue={form.channel}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setForm({ ...form, channel: e.target.value })}
-            >
-              <Flex direction="row" gap="var(--space-5, 20px)">
-                <Radio value={'faststore'} checked={form.channel === 'faststore'}>{t('agent.setup.forms.channel.faststore')}</Radio>
-                <Radio value={'portal'} checked={form.channel === 'portal'}>{t('agent.setup.forms.channel.portal')}</Radio>
-                <Radio value={'site_editor'} checked={form.channel === 'site_editor'}>{t('agent.setup.forms.channel.site_editor')}</Radio>
-              </Flex>
-            </RadioGroup>
+    <>
+      {isSetupLoading ? (
+        <AgentBuilderSkeleton />
+      ) : (
+        <Flex direction="column" gap="var(--space-5, 20px)">
+          <Flex direction="column" gap="var(--space-5, 20px)">
+            <Field error={!!errors.name}>
+              <Label>{t('agent.setup.forms.name')}</Label>
+              <Input
+                name="name"
+                value={form.name}
+                onChange={(e) => handleInputChange('name', e)}
+                disabled={agentIntegrated}
+              />
+              <FieldError>{errors.name}</FieldError>
+            </Field>
+            <Field error={!!errors.knowledge}>
+              <Label style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-05, 2px)' }}>
+                {t('agent.setup.forms.knowledge.title')}
+                <Tooltip label={t('agent.setup.forms.knowledge.context')}>
+                  <Flex style={{ width: '24px', justifyContent: 'center' }}>
+                    <img src={question} alt="" />
+                  </Flex>
+                </Tooltip>
+              </Label>
+              <Input
+                name="knowledge"
+                value={form.knowledge}
+                onChange={(e) => handleInputChange('knowledge', e)}
+                disabled={agentIntegrated}
+                prefix={'https://'}
+              />
+              <FieldError>{errors.knowledge}</FieldError>
+            </Field>
+            <Field>
+              <Label>{t('agent.setup.forms.occupation.title')}</Label>
+              <Input
+                name="occupation"
+                value={form.occupation}
+                onChange={(e) => handleInputChange('occupation', e)}
+                disabled={agentIntegrated}
+              />
+            </Field>
+            <Field>
+              <Label>{t('agent.setup.forms.objective.title')}</Label>
+              <Textarea
+                className="content-textarea-full-width"
+                name="objective"
+                value={form.objective}
+                onChange={(e) => handleInputChange('objective', e)}
+                disabled={agentIntegrated}
+              />
+            </Field>
           </Flex>
-          {isSetupLoading ? (
-            <AgentBuilderSkeleton />
-          ) : (
-            <Flex direction="column" gap="var(--space-5, 20px)">
-              <Flex direction="column" gap="var(--space-5, 20px)">
-                <Field error={!!errors.name}>
-                  <Label>{t('agent.setup.forms.name')}</Label>
-                  <Input
-                    name="name"
-                    value={form.name}
-                    onChange={(e) => handleInputChange('name', e)}
-                    disabled={agentIntegrated}
-                  />
-                  <FieldError>{errors.name}</FieldError>
-                </Field>
-                <Field error={!!errors.knowledge}>
-                  <Label style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-05, 2px)' }}>
-                    {t('agent.setup.forms.knowledge.title')}
-                    <Tooltip label={t('agent.setup.forms.knowledge.context')}>
-                      <Flex style={{ width: '24px', justifyContent: 'center' }}>
-                        <img src={question} alt="" />
-                      </Flex>
-                    </Tooltip>
-                  </Label>
-                  <Input
-                    name="knowledge"
-                    value={form.knowledge}
-                    onChange={(e) => handleInputChange('knowledge', e)}
-                    disabled={agentIntegrated}
-                    prefix={'https://'}
-                  />
-                  <FieldError>{errors.knowledge}</FieldError>
-                </Field>
-                <Field>
-                  <Label>{t('agent.setup.forms.occupation.title')}</Label>
-                  <Input
-                    name="occupation"
-                    value={form.occupation}
-                    onChange={(e) => handleInputChange('occupation', e)}
-                    disabled={agentIntegrated}
-                  />
-                </Field>
-                <Field>
-                  <Label>{t('agent.setup.forms.objective.title')}</Label>
-                  <Textarea
-                    name="objective"
-                    value={form.objective}
-                    onChange={(e) => handleInputChange('objective', e)}
-                    style={{ minWidth: '720px' }}
-                    disabled={agentIntegrated}
-                  />
-                </Field>
-              </Flex>
-              <Text variant="display3">{t('integration.title')}</Text>
-              <Channel isIntegrated={isWppIntegrated} showSkipDisclaimer={!isWppIntegrated} />
-            </Flex>
-          )}
-        </PageContent>
-      </Page>
-    </Container>
+        </Flex>
+      )}
+    </>
   );
 }
