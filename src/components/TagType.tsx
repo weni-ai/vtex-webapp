7ced3d6f6e749b5260fdc565d29527c1101a1717
Matheus Cristian <mathues@email.com>
2025-06-17 15:59:52 -0300

    feat(minor-tasks): adds tag type test file

--- src/components/TagType.tsx
+++ src/components/TagType.tsx
@@ -1,10 +1,12 @@
 import { Tag } from "@vtex/shoreline";
-
+import { useTranslation } from "react-i18next";
 export interface TagTypeProps {
   type: 'active' | 'passive';
 }
 
 export function TagType({ type }: TagTypeProps) {
+  const { t } = useTranslation();
+
   const color = {
     active: 'blue' as const,
     passive: 'purple' as const,
@@ -12,6 +14,7 @@ export function TagType({ type }: TagTypeProps) {
 
   return (
     <Tag
+      data-testid="tag-type"
       color={color}
       variant="secondary"
       style={{
