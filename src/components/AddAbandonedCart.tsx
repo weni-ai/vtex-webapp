c89ffea791a91f2714615c3ed1ff7ef72c3f74d4
Matheus Cristian <mathues@email.com>
2025-06-18 09:21:17 -0300

    feat(abandoned-cart): changes abandoned cart assign modal to ask store type

--- src/components/AddAbandonedCart.tsx
+++ src/components/AddAbandonedCart.tsx
@@ -1,145 +1,64 @@
-import { Button, Flex, Modal, ModalContent, ModalDismiss, ModalHeader, ModalHeading, Text, ModalFooter, IconCopySimple } from "@vtex/shoreline";
-import store from "../store/provider.store";
+import { Button, Flex, Modal, ModalContent, ModalDismiss, ModalFooter, ModalHeader, ModalHeading, Radio, RadioGroup, Text } from "@vtex/shoreline";
+import { ChangeEvent, useState } from "react";
+import { Trans } from "react-i18next";
+import { InputCopyToClipboard } from "./InputCopyToClipboard";
+
 export interface AddAbandonedCartProps {
-    open: boolean,
-    toggleModal: () => void;
-    confirm: () => void;
+  open: boolean,
+  toggleModal: () => void;
+  confirm: () => void;
 }
 
 export function AddAbandonedCart({ open, toggleModal, confirm }: AddAbandonedCartProps) {
-    const channel = store.getState().project.storeType;
-    return (
-        <Modal open={open} onClose={toggleModal} >
-            <ModalHeader>
-                <Flex>
-                    <ModalHeading>
-                        {t('agents.categories.active.abandoned_cart.modal.title')}
-                    </ModalHeading>
-                </Flex>
-                <ModalDismiss />
-            </ModalHeader>
-            <ModalContent>
-                <Flex style={{ marginBottom: 'var(--sl-space-5)' }}>
-                    <Text variant="body">{t('agents.categories.active.abandoned_cart.modal.steps.faststore.description')}</Text>
-                </Flex>
-                {channel === 'faststore' ? (
-                    <Flex direction="column" gap="$space-5">
-                        <Flex direction="column" gap="$space-5">
-                            <Text variant="body" style={{ paddingLeft: '1.5rem', position: 'relative' }}>
-                                <span style={{ position: 'absolute', left: 0 }}>1.</span>
-                                {t('agents.categories.active.abandoned_cart.modal.steps.faststore.step1')}
-                            </Text>
-                        </Flex>
-                        <Flex
-                            style={{
-                                position: 'relative',
-                                backgroundColor: 'var(--sl-color-neutral-100)',
-                                padding: 'var(--sl-space-4)',
-                                borderRadius: 'var(--sl-radius-2)',
-                                marginTop: 'var(--sl-space-2)',
-                                border: '1px solid var(--sl-color-gray-5)',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <pre style={{ margin: 0, whiteSpace: 'pre-wrap' }}>
-                                <code>
-                                    {`<script type="text/javascript" src="https://appvtexio.weni.ai/pixel.js"></script>`}
-                                </code>
-                            </pre>
-                            <Button
-                                variant="tertiary"
-                                style={{
-                                    position: 'absolute',
-                                    right: 'var(--sl-space-2)',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    top: '50%',
-                                    transform: 'translateY(-50%)'
-                                }}
-                                onClick={() => {
-                                    navigator.clipboard.writeText(`<script type="text/javascript" src="https://appvtexio.weni.ai/pixel.js"></script>`);
-                                }}
-                            >
-                                {t('common.copy_code')}
-                                <IconCopySimple
-                                    height="1rem"
-                                    width="1rem"
-                                    display="inline"
-                                    style={{
-                                        marginLeft: 'var(--sl-space-05)'
-                                    }}
-                                />
-                            </Button>
-                        </Flex>
-                    </Flex>
-                ) : (
-                    <Flex direction="column" gap="$space-5">
-                        <Flex direction="column" gap="$space-0">
-                            <Text variant="body" style={{ paddingLeft: '1.5rem', position: 'relative' }}>
-                                <span style={{ position: 'absolute', left: 0 }}>1.</span>
-                                {t('agents.categories.active.abandoned_cart.modal.steps.portal.step1')}
-                            </Text>
-                            <Text variant="body" style={{ paddingLeft: '1.5rem', position: 'relative' }}>
-                                <span style={{ position: 'absolute', left: 0 }}>2.</span>
-                                {t('agents.categories.active.abandoned_cart.modal.steps.portal.step2')}
-                            </Text>
-                            <Text variant="body" style={{ paddingLeft: '1.5rem', position: 'relative' }}>
-                                <span style={{ position: 'absolute', left: 0 }}>3.</span>
-                                {t('agents.categories.active.abandoned_cart.modal.steps.portal.step3')}
-                            </Text>
-                            <Text variant="body" style={{ paddingLeft: '1.5rem', position: 'relative' }}>
-                                <span style={{ position: 'absolute', left: 0 }}>4.</span>
-                                {t('agents.categories.active.abandoned_cart.modal.steps.portal.step4')}
-                            </Text>
-                        </Flex>
-                        <Flex
-                            style={{
-                                position: 'relative',
-                                backgroundColor: 'var(--sl-color-neutral-100)',
-                                padding: 'var(--sl-space-4)',
-                                borderRadius: 'var(--sl-radius-2)',
-                                marginTop: 'var(--sl-space-2)',
-                                border: '1px solid var(--sl-color-gray-5)',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <pre style={{ margin: 0, whiteSpace: 'pre-wrap' }}>
-                                <code>
-                                    {`<script type="text/javascript" src="https://appvtexio.weni.ai/pixel.js"></script>`}
-                                </code>
-                            </pre>
-                            <Button
-                                variant="tertiary"
-                                style={{
-                                    position: 'absolute',
-                                    right: 'var(--sl-space-2)',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    top: '50%',
-                                    transform: 'translateY(-50%)'
-                                }}
-                                onClick={() => {
-                                    navigator.clipboard.writeText(`<script type="text/javascript" src="https://appvtexio.weni.ai/pixel.js"></script>`);
-                                }}
-                            >
-                                {t('common.copy_code')}
-                                <IconCopySimple
-                                    height="1rem"
-                                    width="1rem"
-                                    display="inline"
-                                    style={{
-                                        marginLeft: 'var(--sl-space-05)'
-                                    }}
-                                />
-                            </Button>
-                        </Flex>
-                    </Flex>
-                )}
-            </ModalContent>
-            <ModalFooter>
-                <Button onClick={toggleModal}>{t('common.cancel')}</Button>
-                <Button variant="primary" onClick={confirm}>{t('common.confirm')}</Button>
-            </ModalFooter>
-        </Modal >
-    )
+  const [channel, setChannel] = useState('site_editor');
+
+  return (
+    <Modal open={open} onClose={toggleModal} size="large">
+      <ModalHeader>
+        <Flex>
+          <ModalHeading>
+            {t('agents.categories.active.abandoned_cart.modal.title')}
+          </ModalHeading>
+        </Flex>
+        <ModalDismiss />
+      </ModalHeader>
+
+      <ModalContent>
+        <Flex direction="column">
+          <Text variant="display3">
+            {t('agent.setup.forms.channel.container_title')}
+          </Text>
+
+          <RadioGroup
+            label={t('agent.setup.forms.channel.title')}
+            defaultValue={channel}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setChannel(e.target.value)}
+          >
+            <Flex direction="row" gap="var(--space-5, 20px)">
+              <Radio value={'site_editor'} checked={channel === 'site_editor'}>{t('agent.setup.forms.channel.site_editor')}</Radio>
+              <Radio value={'other'} checked={channel === 'other'}>{t('agent.setup.forms.channel.other')}</Radio>
+            </Flex>
+          </RadioGroup>
+
+          {channel === 'other' && (
+            <>
+              <Text variant="body">
+                <Trans i18nKey={'agent.setup.forms.channel.description'} components={[<br />, <br />, <b />]} />
+              </Text>
+
+              <InputCopyToClipboard
+                value={'<script type="text/javascript" src="https://appvtexio.weni.ai/pixel.js"></script>'}
+                successMessage={t('common.script_copied')}
+              />
+            </>
+          )}
+        </Flex>
+      </ModalContent>
+
+      <ModalFooter>
+        <Button onClick={toggleModal}>{t('common.cancel')}</Button>
+        <Button variant="primary" onClick={confirm}>{t('common.finish')}</Button>
+      </ModalFooter>
+    </Modal >
+  )
 }
