bdc2ea5d74302f3a52a48483181391dbbc67a807
Matheus Cristian <mathues@email.com>
2025-06-25 13:16:05 -0300

    feat(custom-template): adds template name in template form

--- src/pages/template/FormEssential.tsx
+++ src/pages/template/FormEssential.tsx
@@ -1,10 +1,20 @@
 import { ContextualHelp, Field, FieldDescription, Flex, Input, Label } from "@vtex/shoreline";
 import { SectionHeader } from "./Template";
 
-export function FormEssential({ startCondition, isDisabled }: { startCondition: string, isDisabled: boolean }) {
+export function FormEssential({ name, setName, startCondition, setStartCondition, isDisabled }: { name: string, setName: (name: string) => void, startCondition: string, setStartCondition: (startCondition: string) => void, isDisabled: boolean }) {
   return (
     <Flex direction="column" gap="$space-4">
       <SectionHeader title={t('template.form.areas.basic.title')} />
+
+      <Field>
+        <Label>
+          <Flex align="center" gap="$space-05">
+            {t('template.form.fields.name.label')}
+          </Flex>
+        </Label>
+
+        <Input value={name} disabled={isDisabled} onChange={(value) => setName(value)} />
+      </Field>
 
       <Field>
         <Label>
@@ -17,7 +27,7 @@ export function FormEssential({ startCondition, isDisabled }: { startCondition: 
           </Flex>
         </Label>
 
-        <Input value={startCondition} disabled={isDisabled} />
+        <Input value={startCondition} disabled={isDisabled} onChange={(value) => setStartCondition(value)} />
 
         <FieldDescription>{t('template.form.fields.start_condition.description')}</FieldDescription>
       </Field>
