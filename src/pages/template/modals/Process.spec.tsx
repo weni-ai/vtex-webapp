1e84cda821d5b68a86bd28a991e1e3e1fb15c7f2
Matheus Cristian <mathues@email.com>
2025-08-11 18:15:18 -0300

    lint errors

--- src/pages/template/modals/Process.spec.tsx
+++ src/pages/template/modals/Process.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { ProcessModal } from './Process';
 
